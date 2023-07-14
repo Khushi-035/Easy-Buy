ecd218731b2d9d469c929e1b5ae649b565ba38e5
Khushi Sachan <sachan.khushi02@gmail.com>
2023-07-14 17:00:42 +0530

    Temp Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,36 @@
 import { Outlet } from 'react-router-dom';
-// import './App.css';
+import './App.css';
 import Header from './component/Header';
-import toast, { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
 import { setDataProduct } from './redux/productSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch ,useSelector} from 'react-redux';
 
 function App() {
-  const dispatch = useDispatch()
+
+  const dispatch=useDispatch();
   const productData = useSelector((state)=>state.product)
-  
+
   useEffect(()=>{
     (async()=>{
       const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/product`)
       const resData = await res.json()
-      console.log(resData);
-      dispatch(setDataProduct(resData))
+     //console.log(resData)
+     dispatch(setDataProduct(resData))
     })()
-  },[]) 
- 
-  console.log(productData);
+  },[])
 
-
+  //console.log(productData)
   return (
     <>
-      <Toaster />
-      <div>
-      <Header/> 
+    <Toaster/>
+    <div >
+      <Header/>
       <main className='pt-16 bg-slate-100 min-h-[calc(100vh)]'>
-        <Outlet />
+<Outlet/>
       </main>
-      </div>
-    
+    </div>
     </>
-    
   );
 }
 
